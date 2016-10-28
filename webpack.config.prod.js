7182d00628e16d72b91378828a2c06e47070f09e
dmitry.santotsky <dsantotski@thumbtack.net>
2016-10-28 18:57:43 +0300

    added production environment

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,17 +3,18 @@ var webpack = require('webpack')
 var ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
-    devtool: 'source-map',
     entry: [
-        './app/scripts/app'
+        './app/app'
     ],
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'build'),
         filename: 'bundle.js',
-        publicPath: '/'
+        //publicPath: '/'
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin('styles.css',{allChunks:true}),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': "'production'"
@@ -24,7 +25,6 @@ module.exports = {
                 warnings: false
             }
         }),
-        new ExtractTextPlugin('styles.css',{allChunks:true})
     ],
     module: {
         preLoaders: [
@@ -32,7 +32,10 @@ module.exports = {
                 test: /\.js$/,
                 loaders: ['eslint'],
                 include: [
-                    path.resolve(__dirname, "app/scripts"),
+                    path.resolve(__dirname, "app"),
+                ],
+                exclude: [
+                    path.resolve(__dirname, "app/containers"),
                 ],
             }
         ],
@@ -40,19 +43,38 @@ module.exports = {
             {   test: /\.js$/,
                 loaders: ['react-hot', 'babel-loader'],
                 include: [
-                    path.resolve(__dirname, "app/scripts"),
+                    path.resolve(__dirname, "app"),
                 ],
                 test: /\.js$/,
                 plugins: ['transform-runtime'],
             },
             {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(jpg|png|gif)$/i,
+                loader: 'file-loader?name=images/[name].[ext]'
+            },
+            {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader!')
             },
             {
                 test: /\.css$/,
                 loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            }
+            },
         ]
+    },
+    resolve : {
+        extensions: ['', '.js', '.css','.scss'],
+        alias: {
+            bootstrapCss : path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.css'),
+            fontAwesomeCss : path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.css')
+        }
     }
 }
